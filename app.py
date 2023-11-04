749ec1dca0f9b139e8bc35ee224c8de315627217
Konstantin Vyatkin <tino@vtkn.io>
2023-11-04 19:50:12 +0100

    chore: fully working CDK app

--- app.py
+++ app.py
@@ -1,46 +1,130 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of
-# this software and associated documentation files (the "Software"), to deal in
-# the Software without restriction, including without limitation the rights to
-# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-# the Software, and to permit persons to whom the Software is furnished to do so.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from __future__ import annotations
 
 import os
 
 import aws_cdk as cdk
-import aws_cdk.aws_dynamodb as dynamodb
+import aws_cdk.aws_ec2 as ec2
+import aws_cdk.aws_ecs as ecs
+import aws_cdk.aws_iam as iam
+import aws_cdk.aws_logs as logs
+import aws_cdk.custom_resources as cr
 
-import constants
-from backend.component import Backend
-from toolchain import Toolchain
+app = cdk.App(default_stack_synthesizer=cdk.CliCredentialsStackSynthesizer())
 
-app = cdk.App()
+log_group_name = "test-task-group-6"
+stream_prefix = "test-task-stream-5"
+container_name = "python:3.11"
+command = 'pip install pip -U && pip install tqdm && python -c "import time\ncounter = 0\nwhile True:\n\tprint(counter)\n\tcounter = counter + 1\n\ttime.sleep(0.1)"'
 
-# Component sandbox stack
-Backend(
+
+stack = cdk.Stack(
     app,
-    constants.APP_NAME + "Sandbox",
+    "ContainerTask",
     env=cdk.Environment(
-        account=os.environ["CDK_DEFAULT_ACCOUNT"],
-        region=os.environ["CDK_DEFAULT_REGION"],
+        account=os.environ.get("CDK_DEFAULT_ACCOUNT"),
+        region=os.environ.get("CDK_DEFAULT_REGION"),
     ),
-    api_lambda_reserved_concurrency=1,
-    database_dynamodb_billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
 )
 
-# Toolchain stack (defines the continuous deployment pipeline)
-Toolchain(
-    app,
-    constants.APP_NAME + "Toolchain",
-    env=cdk.Environment(account="111111111111", region="eu-west-1"),
+vpc = ec2.Vpc.from_lookup(stack, "VPC", is_default=True)
+cluster = ecs.Cluster(stack, "LoggingCluster", vpc=vpc, enable_fargate_capacity_providers=True)
+
+
+log_group_upsert = cr.AwsCustomResource(
+    stack,
+    "LogGroupUpsert",
+    resource_type="Custom::LogGroupUpsert",
+    on_create=cr.AwsSdkCall(
+        service="CloudwatchLogs",
+        action="createLogGroup",
+        parameters={"logGroupName": log_group_name},
+        ignore_error_codes_matching="ResourceAlreadyExistsException",
+        physical_resource_id=cr.PhysicalResourceId.of(log_group_name),
+    ),
+    policy=cr.AwsCustomResourcePolicy.from_sdk_calls(resources=cr.AwsCustomResourcePolicy.ANY_RESOURCE),
 )
+log_group = logs.LogGroup.from_log_group_name(
+    stack,
+    "LogGroup",
+    log_group_name=log_group_name,
+)
+log_group.node.add_dependency(log_group_upsert)
+
+
+task_definition = ecs.FargateTaskDefinition(stack, "TaskDefinition")
+task_definition.add_container(
+    "Container",
+    image=ecs.ContainerImage.from_registry(container_name),
+    command=[
+        "/bin/sh",
+        "-c",
+        command,
+    ],
+    logging=ecs.AwsLogDriver(
+        log_group=log_group,
+        stream_prefix=stream_prefix,
+        mode=ecs.AwsLogDriverMode.NON_BLOCKING,
+        max_buffer_size=cdk.Size.kibibytes(1),  # default is 1MB, we set it smaller to see logs faster
+    ),
+)
+
+# running the task
+start_task = cr.AwsSdkCall(
+    service="ECS",
+    action="runTask",
+    parameters={
+        "capacityProviderStrategy": [
+            {"capacityProvider": "FARGATE_SPOT", "weight": 2},
+            {"capacityProvider": "FARGATE", "weight": 1},
+        ],
+        "cluster": cluster.cluster_arn,
+        "count": 1,
+        "taskDefinition": task_definition.task_definition_arn,
+        "networkConfiguration": {
+            "awsvpcConfiguration": {
+                "subnets": [s.subnet_id for s in vpc.public_subnets],
+                "assignPublicIp": "ENABLED",
+            }
+        },
+    },
+    physical_resource_id=cr.PhysicalResourceId.from_response("tasks.0.taskArn"),
+)
+task_runner = cr.AwsCustomResource(
+    stack,
+    "TaskRunner",
+    resource_type="Custom::FargateTaskRunner",
+    on_create=start_task,
+    on_update=start_task,
+    on_delete=cr.AwsSdkCall(
+        service="ECS",
+        action="stopTask",
+        parameters={
+            "cluster": cluster.cluster_arn,
+            "task": cr.PhysicalResourceIdReference(),
+        },
+        ignore_error_codes_matching="InvalidParameterException",  # task already stopped
+    ),
+    policy=cr.AwsCustomResourcePolicy.from_statements(
+        [
+            iam.PolicyStatement(
+                resources=["*"],
+                actions=["ECS:RunTask", "ECS:StopTask"],
+            ),
+            iam.PolicyStatement(
+                resources=[
+                    task_definition.task_role.role_arn,
+                    task_definition.execution_role.role_arn,
+                ],
+                actions=["iam:PassRole"],
+            ),
+        ]
+    ),
+)
+task_runner.node.add_dependency(task_definition)
+
+cdk.CfnOutput(stack, "ClusterName", value=cluster.cluster_name)
+cdk.CfnOutput(stack, "TaskDefinitionArn", value=task_definition.task_definition_arn)
+cdk.CfnOutput(stack, "StartedTaskArn", value=task_runner.get_response_field("tasks.0.taskArn"))
+
 
 app.synth()
